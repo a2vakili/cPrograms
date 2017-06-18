a1948234b43e07707d7c3b66f62576dee12621e5
Arsalan Vakili <arsenal.v.001@gmail.com>
2017-06-18 14:51:22 -0400

    Initial Commit

---
+++ credit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  credit
+//
+//  Created by Arsalan Vakili on 2017-06-18.
+//  Copyright © 2017 Arsalan Vakili. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
